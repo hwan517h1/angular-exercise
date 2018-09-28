cfe0de9bd54cfd556eb459de8831797dece40e93
hwan517h1 <hwan517h1@gmail.com>
2018-09-29 02:08:39 +0900

    Implement Service using @Injectable() and ngOnInit's lifecycle

--- SWPP/angular-tour-of-heroes-2/src/app/heroes/heroes.component.ts
+++ SWPP/angular-tour-of-heroes-2/src/app/heroes/heroes.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  // Leave constructor as empty as possible
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
@@ -28,7 +32,13 @@ export class HeroesComponent implements OnInit {
     }
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   ngOnInit() {
+    // Use Lifecycle of ngOnInit()
+    this.getHeroes();
   }
 
 }
