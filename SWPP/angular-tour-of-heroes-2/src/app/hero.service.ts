7c6ab95a77e36be1c339bd4a7554b72acd9691ad
hwan517h1 <hwan517h1@gmail.com>
2018-09-30 01:29:52 +0900

    Use RxJS

--- SWPP/angular-tour-of-heroes-2/src/app/hero.service.ts
+++ SWPP/angular-tour-of-heroes-2/src/app/hero.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
 
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -14,7 +16,11 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // Send the message after fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return of(HEROES).pipe(
+      delay(1000),
+      map((heroes: Hero[]) => heroes.map((hero: Hero) => ({...hero, initial: false}))),
+    );
   }
 }
